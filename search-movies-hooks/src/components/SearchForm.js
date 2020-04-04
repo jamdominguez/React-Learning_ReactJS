b0085e929adcc507f0795137699b40a44e36981d
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2020-04-05 01:27:27 +0200

    customize style with material-ui applied customize style class to SearchForm.js

--- search-movies-hooks/src/components/SearchForm.js
+++ search-movies-hooks/src/components/SearchForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { Container, Card, Grid, Typography, TextField, Button } from '@material-ui/core'
+import { Container, Card, Grid, Typography, TextField, Button, makeStyles } from '@material-ui/core'
+import styles from '../styles'
 
 export default () => {
     const [textToSearch, setTextToSearch] = useState('') // Hook, works like this.state and setState. Initialize the state
+    const classes = styles();
     
     const handleSetTextToSearch = (e) => {
             setTextToSearch(e.target.value)            
@@ -16,24 +18,25 @@ export default () => {
         console.log('handleSeachOnClik', textToSearch)
     }
     return(        
-        <Container>
-            <Card>
-                <Grid container>
+        <Container className={classes.container}>
+            <Card className={classes.cardContainer}>
+                <Grid container className={classes.titleGridContainer}>
                     <Grid>
-                        <Typography>Welcome</Typography>
+                        <Typography className={classes.title}>Welcome</Typography>
                     </Grid>
                     <Grid>
                         <label></label>
                     </Grid>
                 </Grid>
                 <TextField
+                    className={classes.textFieldSearch}
                     onChange={handleSetTextToSearch}
                     placeholder='Search...'
                     value={textToSearch}
                 />
-                <Grid>
+                <Grid className={classes.buttonsContainer}>
                     <Button onClick={handleCleanOnClik} variant='contained'>Clean</Button>
-                    <Button onClick={handleSeachOnClik} variant='contained' color='primary'>Search</Button>
+                    <Button className={classes.searchButton} color='primary' onClick={handleSeachOnClik} variant='contained'>Search</Button>
                 </Grid>
             </Card>
         </Container>
